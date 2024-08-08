2e77304d47983473b1e8d0cb585b23099a8febf5
heisonyee <heisonyee@126.com>
2024-08-08 16:08:38 +0800

    dd

--- kafka/segmentio/registrar/registrar.go
+++ kafka/segmentio/registrar/registrar.go
@@ -1,1 +1,72 @@
 package registrar
+
+import (
+	"context"
+
+	"go.temporal.io/sdk/client"
+)
+
+type RegisterFunc func(context.Context, ServiceRegistrar) error
+
+type WithRegistrar interface {
+	TemporalRegistrar(context.Context) RegisterFunc
+}
+
+type ServiceRegistrar interface {
+	client.Client
+}
+
+type Func[S any] func(ServiceRegistrar, S)
+
+type FuncWithErr[S any] func(ServiceRegistrar, S) error
+
+type CtxFunc[S any] func(context.Context, ServiceRegistrar, S)
+
+type CtxFuncWithErr[S any] func(context.Context, ServiceRegistrar, S) error
+
+type Registrar interface {
+	RegisterToTemporal(context.Context, ServiceRegistrar) error
+}
+
+type registrar[S any] struct {
+	s S
+	f CtxFuncWithErr[S]
+}
+
+var _ Registrar = (*registrar[any])(nil)
+
+func (h *registrar[S]) RegisterToTemporal(ctx context.Context, r ServiceRegistrar) error {
+	return h.f(ctx, r, h.s)
+}
+
+func New[S any](s S, f Func[S]) Registrar {
+	cf := func(ctx context.Context, r ServiceRegistrar, s S) error {
+		f(r, s)
+		return nil
+	}
+	return NewCtxWithErr(s, cf)
+}
+
+func NewWithErr[S any](s S, f Func[S]) Registrar {
+	cf := func(ctx context.Context, r ServiceRegistrar, s S) error {
+		f(r, s)
+		return nil
+	}
+	return NewCtxWithErr(s, cf)
+}
+
+func NewCtx[S any](s S, f CtxFunc[S]) Registrar {
+	cf := func(ctx context.Context, r ServiceRegistrar, s S) error {
+		f(ctx, r, s)
+		return nil
+	}
+	return NewCtxWithErr(s, cf)
+}
+
+func NewCtxWithErr[S any](s S, f CtxFuncWithErr[S]) Registrar {
+	h := &registrar[S]{
+		s: s,
+		f: f,
+	}
+	return h
+}
