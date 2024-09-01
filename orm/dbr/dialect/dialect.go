a31603cfe64cd7bbf9916b4d74ffc120e0c87211
heisonyee <heisonyee@126.com>
2024-09-01 23:58:45 +0800

    eee

--- orm/dbr/dialect/dialect.go
+++ orm/dbr/dialect/dialect.go
@@ -1,1 +1,17 @@
 package dialect
+
+import (
+	"strings"
+)
+
+var (
+	SQLite3 = sqlite3{}
+)
+
+func QuoteIdent(s, quote string) string {
+	part := strings.SplitN(s, ".", 2)
+	if len(part) == 2 {
+		return QuoteIdent(part[0], quote) + "." + QuoteIdent(part[1], quote)
+	}
+	return quote + s + quote
+}
--- tests/orm/dbr_test.go
+++ tests/orm/dbr_test.go
@@ -3,68 +3,31 @@ package orm
 import (
 	"fmt"
 	"log/slog"
-	"strings"
 	"testing"
 	"time"
 
+	"github.com/blink-io/x/orm/dbr/dialect"
 	dbrslog "github.com/blink-io/x/orm/dbr/logger/slog"
 	"github.com/gocraft/dbr/v2"
 	"github.com/guregu/null/v5"
 	"github.com/stretchr/testify/require"
 )
 
-func quoteIdent(s, quote string) string {
-	part := strings.SplitN(s, ".", 2)
-	if len(part) == 2 {
-		return quoteIdent(part[0], quote) + "." + quoteIdent(part[1], quote)
-	}
-	return quote + s + quote
-}
-
-type sqlite3 struct{}
-
-func (d sqlite3) QuoteIdent(s string) string {
-	return quoteIdent(s, `"`)
-}
-
-func (d sqlite3) EncodeString(s string) string {
-	// https://www.sqlite.org/faq.html
-	return `'` + strings.Replace(s, `'`, `''`, -1) + `'`
-}
-
-func (d sqlite3) EncodeBool(b bool) string {
-	// https://www.sqlite.org/lang_expr.html
-	if b {
-		return "1"
-	}
-	return "0"
-}
-
-func (d sqlite3) EncodeTime(t time.Time) string {
-	// https://www.sqlite.org/lang_datefunc.html
-	return `'` + t.Local().Format(time.RFC3339Nano) + `'`
-}
-
-func (d sqlite3) EncodeBytes(b []byte) string {
-	// https://www.sqlite.org/lang_expr.html
-	return fmt.Sprintf(`X'%x'`, b)
-}
-
-func (d sqlite3) Placeholder(_ int) string {
-	return "?"
-}
-
-func TestDBR_1(t *testing.T) {
+func getDBRConn() *dbr.Connection {
 	sl := dbrslog.New(slog.Default(), slog.LevelInfo)
 
 	db := getSqliteDB()
-	dd := sqlite3{}
+	dd := dialect.SQLite3
 	cc := &dbr.Connection{
 		DB:            db,
 		EventReceiver: sl,
 		Dialect:       dd,
 	}
 
+	return cc
+}
+func TestDBR_1(t *testing.T) {
+	cc := getDBRConn()
 	sess := cc.NewSession(nil)
 
 	m := new(Model)
@@ -79,15 +42,7 @@ func TestDBR_1(t *testing.T) {
 }
 
 func TestDBR_User_Insert_1(t *testing.T) {
-	sl := dbrslog.New(slog.Default(), slog.LevelInfo)
-
-	db := getSqliteDB()
-	dd := sqlite3{}
-	cc := &dbr.Connection{
-		DB:            db,
-		EventReceiver: sl,
-		Dialect:       dd,
-	}
+	cc := getDBRConn()
 
 	sess := cc.NewSession(nil)
 
