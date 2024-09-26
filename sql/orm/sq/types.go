fbf37daed8a412ec0ac8a80954d46964a64528eb
heisonyee <heisonyee@126.com>
2024-09-26 23:37:19 +0800

    eeedd

---
+++ sql/orm/sq/types.go
@@ -0,0 +1,8 @@
+package sq
+
+import "github.com/bokwoon95/sq"
+
+type InsertMapper func() sq.InsertQuery
+type UpdateMapper func() sq.UpdateQuery
+type DeleteMapper func() sq.DeleteQuery
+type QueryMapper[T any] func() (sq.SelectQuery, func(*sq.Row) T)
