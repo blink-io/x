72707fbd1dcb41f65c979b5491f8638ad224ac9e
heisonyep <heisonyee@126.com>
2025-06-22 15:00:49 +0800

    ddd

---
+++ http/router/chi/chi.go
@@ -0,0 +1,9 @@
+package chi
+
+import (
+	"github.com/go-chi/chi/v5"
+)
+
+type (
+	Router = chi.Router
+)
---
+++ http/router/fiber/fiber.go
@@ -0,0 +1,9 @@
+package fiber
+
+import (
+	"github.com/gofiber/fiber/v3"
+)
+
+type (
+	App = fiber.App
+)
