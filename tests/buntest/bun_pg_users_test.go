0824050747c84344a017496bdd15d11c5483897a
heisonyep <heisonyee@126.com>
2025-07-09 00:15:51 +0800

    ddd

--- tests/buntest/bun_pg_users_test.go
+++ tests/buntest/bun_pg_users_test.go
@@ -1,6 +1,9 @@
 package buntest
 
 import (
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/pgdialect"
+	"golang.org/x/net/context"
 	"testing"
 	"time"
 
@@ -9,7 +12,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestPg_Users_SelectJoin_1(t *testing.T) {
+func TestPg_Squirrel_Insert_1(t *testing.T) {
 	db := getPgDB()
 	cols := []string{
 		TblSimpleTable.Columns.GUID,
@@ -28,4 +31,34 @@ func TestPg_Users_SelectJoin_1(t *testing.T) {
 		PlaceholderFormat(sq.Dollar).
 		Exec()
 	require.Nil(t, err)
+}
+
+func TestPg_Users_SelectJoin_1(t *testing.T) {
+	db := getPgDB()
+	bundb := bun.NewDB(db, pgdialect.New())
+	ctx := context.Background()
+
+	type UserWithDevices struct {
+		UserID        int64  `bun:"user_id"`
+		UserFirstName string `bun:"user_first_name"`
+		UserLastName  string `bun:"user_last_name"`
+		UserDeviceID  int64  `bun:"user_device_id"`
+		DeviceModel   string `bun:"device_model"`
+		DeviceName    string `bun:"device_name"`
+	}
+
+	t.Run("users join user_devices", func(t *testing.T) {
+		var records []UserWithDevices
+		err := bundb.NewSelect().
+			ColumnExpr("u.id as user_id").
+			ColumnExpr("u.first_name as user_first_name").
+			ColumnExpr("u.last_name as user_last_name").
+			ColumnExpr("d.id as user_device_id").
+			ColumnExpr("d.model as device_model").
+			ColumnExpr("d.name as device_name").
+			TableExpr("users u").
+			Join("join user_devices d on u.id = d.user_id").Scan(ctx, &records)
+		require.NoError(t, err)
+	})
+
 }
